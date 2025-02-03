62e2218fd3a846fa736793ecadd6f154a2a31ec4
tete91 <128594944+tete91@users.noreply.github.com>
2025-02-03 17:07:15 +0100

    Add files via upload

---
+++ principal.java
@@ -0,0 +1,13 @@
+package ejerciciopropuestofor2;
+
+public class principal {
+
+	public static void main(String[] args) {
+		//Escribiremos "Feliz Navidad año "n"." desde 1990 hasta 2007
+		//Primero inicializaremos las variables a utilizar.
+		int ano;//Variable que incluirá el año a imprimir.
+		for(ano=1990;ano<=2007;ano++) {//Bucle que inicia en 1990 y finaliza en 2007 con incremento de 1 ascendente.
+			System.out.println("Feliz Navidad año "+ano+".");//Imprimimos el mensaje por pantalla y el año.
+		}
+	}
+}
