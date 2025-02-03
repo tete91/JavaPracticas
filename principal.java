6e7c7c6ca7008afa72f431a6c5d288fbc494a0b9
tete91 <128594944+tete91@users.noreply.github.com>
2025-02-03 17:08:29 +0100

    Add files via upload

--- principal.java
+++ principal.java
@@ -1,16 +1,21 @@
-package ejerciciopropuestofor3;
+package ejerciciopropuestofor4;
 
 public class principal {
 
 	public static void main(String[] args) {
-		//Escribiremos la tabla de multiplicar del 5
-		//Primero inicializamos las variables a utilizar.
-		int num=0;//Variable que almacenara el numero a imprimir.
-		int cont;//Variable que contara las vueltas y al mismo tiempo dara el numero por el que se multiplica el 5 en la tabla.
-		for(cont=1;cont<=10;cont++) {//Bucle que seguira mientras el contador sea igual o menor a 10 ya que es donde acaba la tabla, empieza en 1 que es donde empieza
-									 //la misma y es ascendente de 1
-			num=5*cont;//Formula para calcular el resultado de la multiplicacion
-			System.out.println("5 por "+cont+" = "+num);//Imprimimos resultado por terminal.
+		//Escribiremos el factorial del numero 15
+		//Inicializamos las variables a utilizar.
+		int confact;//Variable que contendra el numero a multiplicar asi como el numero regresivo del bucle.
+		int numfac=15;//Variable que contiene el numero del cual se busca el factor, lo he separado del bucle para que sea mas facil cambiar el numero a buscar.
+		Float fact=(float)numfac;//Variable que calculara el resultado de la factorial, lo indicamos en float pues no sabemos cuan larga sea la cadena
+		  						 //despues parsearemos a int por terminal.
+								 //La inicializamos con el valor a factorizar haciendo parseo.
+		for(confact=numfac;confact>1;confact--) {//Bucle for que inicia en el numero del cual se busca el factorial. y regresa en orden descendente hasta 1 para evitar
+								  			     //calculos innecesarios.
+			fact=fact*confact;
 		}
+		System.out.println("El factorial del numero "+numfac+" es: "+fact);
+	
+	
 	}
 }
