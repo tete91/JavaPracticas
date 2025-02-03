a3b2fa71f17acebe03d4a61073a528d30ac57bbc
tete91 <128594944+tete91@users.noreply.github.com>
2025-02-03 13:19:14 +0100

    Update and rename principal.java to SumarNumsArray.java

--- SumarNumsArray.java
+++ SumarNumsArray.java
@@ -9,20 +9,12 @@ public class principal {
 		//Este programa sumara todos los numeros incluidos dentro de un array
 		//El array sera el: float[] decimales = new float[]{3.4F,5.67F,12.0F,3.141615F,0.0F};
 		//Primero inicializamos el array con las variables a sumar y las variables necesarias.
-		/*
-	 	float[] decimales = new float[]{3.4F,5.67F,12.0F,3.141615F,0.0F};//Array a sumar sus numeros
-		float resultado=0;//Variable que almacenará la suma
-		int contador=0;//Contador para el bucle for
-		for(contador=0; contador<decimales.length; contador++) {//Bucle que recorrera todo el array
-			resultado=resultado+decimales[contador];//Operacion de suma de cada valor en cada posicion.
-		}
-		System.out.println("Los numeros a sumar son: "+Arrays.toString(decimales));//Impresion del array
-		System.out.println("El resultado de la suma de los numeros es: "+resultado);//Impresion del resultado
-		*/
 	 	float[] decimales = new float[]{3.4F,5.67F,12.0F,3.141615F,0.0F};//Array a sumar sus numeros
 		float resultado=0;//Variable que almacenará la suma
 		for(float numero:decimales) {//Bucle que recorrerá todo el array decimales e insertando sus valores en la variable numero
 			resultado=resultado+numero;//Operacion de suma de cada valor en cada posicion.
 		}
+		System.out.println("Los numeros a sumar son: "+Arrays.toString(decimales));//Impresion del array
+		System.out.println("El resultado de la suma de los numeros es: "+resultado);//Impresion del resultado
 	}
 }
