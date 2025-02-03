313c106544402d053c151b5fea8ff42db61d84ce
tete91 <128594944+tete91@users.noreply.github.com>
2025-02-03 17:14:09 +0100

    Add files via upload

--- principal.java
+++ principal.java
@@ -1,14 +1,14 @@
-package ejerciciopropuestowhile3;
+package ejerciciopropuestowhile4;
 
 public class principal {
 
 	public static void main(String[] args) {
-		//A continuacion calcularemos los 25 primeros numeros primos, que no del 1 al 25.
+		//encontraremos el primer número primo a partir de 198
 		//Primero inicializamos las variables a utilizar.
-		//el numprimo lo inicializamos desde el primer numero primo para ahorrar calculos innecesarios.
-		int numprimo=2;//Esta variable servira para ir verificando si el numero es primo a medida que lo aumentemos, y para imprimirlo en caso de serlo.
+		//el numprimo lo inicializamos desde el 198 para calcular los primos solo desde ahi.
+		int numprimo=198;//Esta variable servira para ir verificando si el numero es primo a medida que lo aumentemos, y para imprimirlo en caso de serlo.
 		int contprimo=0;//Variable para contar las vueltas de numeros primos encontrados
-		while(contprimo<25){//Bucle que seguirá hasta encontrar 25 primos contando que inicializa en 0.
+		while(contprimo<1){//Bucle que seguirá hasta encontrar 1 primo contando que inicializa en 0.
 			int contdiv=0;//Variable que contara por cuantos numeros es divisible un numero.
 			for(int n=1; n<=numprimo;n++) {//Bucle que se repetirá mientras la variable n sea menor al numero a comprobar.
 				if(numprimo%n==0) {//Si el resto del numero comprobado con el numero divido da 0 es que es divisible por el y sube el contador contdiv.
